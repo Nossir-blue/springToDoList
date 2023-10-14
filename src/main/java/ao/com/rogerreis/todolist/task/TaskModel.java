9701262fb734a103213101a6706d5c22940369d7
Nossir-blue <rogerreis20@protonmail.com>
2023-10-14 01:30:04 +0100

    2nd change

--- src/main/java/ao/com/rogerreis/todolist/task/TaskModel.java
+++ src/main/java/ao/com/rogerreis/todolist/task/TaskModel.java
@@ -1,4 +1,4 @@
-package ao.com.rogerreis.todolist.user;
+package ao.com.rogerreis.todolist.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,18 +12,22 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name = "tb_users")
-public class UserModel {
-
+@Entity(name = "tb_tasks")
+public class TaskModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+    private String description;
 
-    @Column(unique = true)
-    private String username;
-    private String name;
-    private String password;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
 
     @CreationTimestamp
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
+
 }
