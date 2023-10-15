8a1fd6705845c417da7dd25be75acae3170b4e1e
Nossir-blue <rogerreis20@protonmail.com>
2023-10-15 15:57:37 +0100

    deploy todo

--- src/main/java/ao/com/rogerreis/todolist/filter/FilterTaskAuth.java
+++ src/main/java/ao/com/rogerreis/todolist/filter/FilterTaskAuth.java
@@ -20,23 +20,26 @@ public class FilterTaskAuth extends OncePerRequestFilter{
 
     @Autowired
     private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                //get auth (usuário e senha)
-                var authorization = request.getHeader("Authorization");
+                var servletPath = request.getServletPath();
+                if(servletPath.startsWith("/tasks/")){
+                    //get auth (usuário e senha)
+                    var authorization = request.getHeader("Authorization");
 
-                var authEncoded = authorization.substring("Basic".length()).trim();
+                    var authEncoded = authorization.substring("Basic".length()).trim();
 
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+                    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-                var authString = new String(authDecode);
+                    var authString = new String(authDecode);
 
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
-                //validar user
+                    String[] credentials = authString.split(":");
+                    String username = credentials[0];
+                    String password = credentials[1];
+                    //validar user
                     var user = this.userRepository.findByUsername(username);
                     if(user == null){
                         response.sendError(401);
@@ -45,13 +48,18 @@ public class FilterTaskAuth extends OncePerRequestFilter{
                         //validar senha
                         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                         if(passwordVerify.verified){
+                            request.setAttribute("idUser", user.getId());
                             filterChain.doFilter(request, response);
                         } else{
                             response.sendError(401);
                         }
                         //go on with yo life
-                        filterChain.doFilter(request, response);
                     }
+                } else{
+                    filterChain.doFilter(request, response);
+                }
+
+                
                 
     }
 
--- src/main/java/ao/com/rogerreis/todolist/task/ITaskRepository.java
+++ src/main/java/ao/com/rogerreis/todolist/task/ITaskRepository.java
@@ -2,8 +2,11 @@ package ao.com.rogerreis.todolist.task;
 
 import java.util.UUID;
 
+//import org.hibernate.mapping.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
-    
+    List<TaskModel> findByIdUser(UUID idUser);
+    TaskModel findByIdAndUser(UUID id, UUID idUser);
 }
